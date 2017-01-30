79ca5eb1267f82cb76d4445f52783830e23f0421
Stephen Larsen <sdlarsen@ualberta.ca>
2017-01-30 14:04:56 -0700

    added gitignore, added clean to Makefile, (kindof) fixed findpattern.c

--- a1/findpattern.c
+++ a1/findpattern.c
@@ -19,7 +19,7 @@ void register_handler(void)
 {
         struct sigaction saSigSegV;     // re-register
         saSigSegV.sa_sigaction = (segfault_handler);
-        saSigSegV.sa_flags = SA_RESTART | SA_SIGINFO | SA_RESETHAND;
+        saSigSegV.sa_flags = SA_RESTART | SA_SIGINFO | SA_RESETHAND | SA_NODEFER;
 
         if(sigaction(SIGSEGV, &saSigSegV, NULL) < 0)
         {
@@ -35,45 +35,45 @@ void register_handler(void)
 void segfault_handler(int sig, siginfo_t* sigInfo, void* ucontext)
 {
         register_handler();
-	siglongjmp(env, 1); 
+	siglongjmp(env, 1);
 }
 
-unsigned int findpattern(unsigned char *pattern, 
+unsigned int findpattern(unsigned char *pattern,
 			unsigned int patlength,
-			struct patmatch *locations, 
-			unsigned int loclength) 
+			struct patmatch *locations,
+			unsigned int loclength)
 {
 	BYTE *mem_ptr, *page_ptr;
 	BYTE test, mode = 0;
 
 	register_handler();
 
-	for ( page_ptr = (void *) (unsigned long) 0; (unsigned long) page_ptr < MAX_ADDRESS; 
+	for ( page_ptr = (void *) (unsigned long) 0; (unsigned long) page_ptr < MAX_ADDRESS;
 			page_ptr += (unsigned long)  getpagesize() )
 	{
 		mem_ptr = page_ptr;
-	
+
 		status = sigsetjmp(env, 0);	// status == SUCCESS
 		printf("Address %x\tStatus %d\n", (int) (unsigned long) mem_ptr, status);
-		
+
 
 		if (status == FAILURE) continue;// memory not readable
 
 		register_handler();
 		test = *mem_ptr;		// read test
 
-		//status = sigsetjmp(env, 0);
-		//mode = MEM_RO;
+		status = sigsetjmp(env, 0);
+		mode = MEM_RO;
 
-		//if (status == SUCCESS)
-		//{
-		//	test = *mem_ptr;	// write test
-		//	*mem_ptr  = 0;
-		//	*mem_ptr = test;
-		//	mode = MEM_RW;
-		//}
+		if (status == SUCCESS)
+		{
+			test = *mem_ptr;	// write test
+			*mem_ptr  = 0;
+			*mem_ptr = test;
+			mode = MEM_RW;
+		}
 
 	}
-	
+
 	return 0;
 }
