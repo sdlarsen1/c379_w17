b6fa1ed0213d72369e75abdbbe27a4b71b9af314
Stephen Larsen <sdlarsen@ualberta.ca>
2017-02-04 14:28:09 -0700

    created template for driver3

---
+++ a1/driver3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "findpattern.h"
+#include <string.h>
+#include <sys/mman.h>
+
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+// https://gcc.gnu.org/onlinedocs/cpp/Stringification.html
+#define STRINGIFY(s) XSTRINGIFY(s)
+#define XSTRINGIFY(s) #s
+#ifndef P
+#define P ""
+#endif
+
+
+int main(int argc, char *argv[]) {
+
+    char *pattern = STRINGIFY(P);
+    int loclength = 16, patlength, patterns_found, i;
+    struct patmatch *locations = malloc(sizeof(struct patmatch) * loclength);
+    struct patmatch *loc;
+    patlength = strlen(pattern);
+
+    printf("Test 3\n");
+    printf("Memory modified using mmap().\n");
+    printf("The pattern is: %s", pattern);
+
+    printf("\nPass 1\n");
+    // The size of pattern is always the same   >!!!!!!!< this is a pointer
+    patterns_found = findpattern(pattern, patlength, locations, loclength);
+    printf("Found %d instances of the pattern.\n", patterns_found);
+
+
+
+    // start 2nd pass
+    printf("\nPass 2\n");
+    // patterns_found = findpattern(pattern, patlength, locations, loclength);
+    patterns_found = findpattern(pattern, patlength, locations, loclength);
+    printf("Found %d instances of the pattern.\n", patterns_found);
+
+    // print out where we found the pattern.
+    for (loc = locations, i = 0; i < MIN(loclength, patterns_found); i++, loc++) {
+        printf("Pattern found at %.8x, ", loc->location);
+        if (loc->mode == 0) {
+            printf("Mode: MEM_RW\n");
+        } else {
+            printf("Mode: MEM_RO\n");
+        }
+    }
+    printf("pattern length: %d\n", patlength);
+
+    return 0;
+}
