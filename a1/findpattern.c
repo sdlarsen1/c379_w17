2b74e6764d88668d349b7dd04bd748230af29225
Hayden Bauder <bauder@cs.ualberta.ca>
2017-02-05 14:36:07 -0700

    minor formatting changes again

--- a1/findpattern.c
+++ a1/findpattern.c
@@ -11,12 +11,13 @@
 typedef unsigned char BYTE;
 
 jmp_buf env;
-enum {SUCCESS, FAILURE} status;
+enum {SUCCESS, FAILURE} status;	
 
-
+/* This signal handler re-registers itself, then executes siglongjmp, 
+	which will set the variable "status" to FAILURE*/
 void segfault_handler(int sig, siginfo_t* sigInfo, void* ucontext)
 {
-    struct sigaction saSigSegV;     // re-register
+    struct sigaction saSigSegV;     // re-register signal handler
     saSigSegV.sa_sigaction = (segfault_handler);
     saSigSegV.sa_flags = SA_RESTART | SA_SIGINFO | SA_RESETHAND;
 
@@ -25,7 +26,7 @@ void segfault_handler(int sig, siginfo_t* sigInfo, void* ucontext)
             perror("ERROR: registering SigFault_Handler\n");
             exit(1);
     }
-	siglongjmp(env, 1);
+    siglongjmp(env, 1);
 }
 
 unsigned int findpattern(unsigned char *pattern,
@@ -33,35 +34,35 @@ unsigned int findpattern(unsigned char *pattern,
 			struct patmatch *locations,
 			unsigned int loclength)
 {
-    BYTE *mem_ptr, *page_ptr;
-    BYTE test, mode = 0;
-    unsigned int num_patterns_found = 0;
+	BYTE *mem_ptr, *page_ptr;
+  	BYTE test, mode = 0;
+  	unsigned int num_patterns_found = 0;
 
 
 
-    struct sigaction saSigSegV;     // re-register
-    saSigSegV.sa_sigaction = (segfault_handler);
-    saSigSegV.sa_flags = SA_RESTART | SA_SIGINFO | SA_RESETHAND;
+    	struct sigaction saSigSegV;     // set up out signal handler
+    	saSigSegV.sa_sigaction = (segfault_handler);
+   	 saSigSegV.sa_flags = SA_RESTART | SA_SIGINFO | SA_RESETHAND;
 
-    if(sigaction(SIGSEGV, &saSigSegV, NULL) < 0)
-    {
-            perror("ERROR: registering SigFault_Handler\n");
-            exit(1);
-    }
+   	 if(sigaction(SIGSEGV, &saSigSegV, NULL) < 0)
+  	  {
+    	        perror("ERROR: registering SigFault_Handler\n");
+ 	           exit(1);
+ 	   }
 
 	for ( page_ptr = (void *) (unsigned long) 0; 
 			(unsigned long) page_ptr < MAX_ADDRESS - getpagesize();
 			page_ptr += (unsigned long)  getpagesize() )
 	{
-		mem_ptr = page_ptr;
+		mem_ptr = page_ptr;	// start of page
 
-		status = sigsetjmp(env, 1);	// status == SUCCESS
+		status = sigsetjmp(env, 1);	// status == FAILURE when we segfault
 		
-		if (status == FAILURE) continue;// memory not readable
+		if (status == FAILURE) continue;// memory not readable? next page
 
 		test = *mem_ptr;		// read test
 
-		status = sigsetjmp(env, 1);
+		status = sigsetjmp(env, 1);     
 		mode = MEM_RO;
 
 		if (status == SUCCESS)
@@ -79,34 +80,13 @@ unsigned int findpattern(unsigned char *pattern,
 		int num_char_match = 0;
 		for (;mem_ptr < page_ptr + getpagesize(); mem_ptr++)
 		{
-		  if (*mem_ptr == pattern[num_char_match])
-		  {
-		    num_char_match++;
-
-
-		    if (num_char_match == patlength)
-		      {
-			// add the pattern
-			if (num_patterns_found < loclength)
-			{
-				locations->location = (unsigned int) (mem_ptr - patlength) + 1;
-				locations->mode = mode;
-				locations++;
-			}
-			num_patterns_found++;
-			num_char_match = 0;
-		      }
-		  }
-		  else    // pattern broke, but we need to check if
-			  // this is the start of a new pattern.
-		  {
-		    num_char_match = 0;
 		    if (*mem_ptr == pattern[num_char_match])
 		    {
-			num_char_match++;
+		        num_char_match++;
 
-		        if (num_char_match == patlength)
-		        {
+
+		        if (num_char_match == patlength) // found the pattern
+		            {
 			    // add the pattern
 			    if (num_patterns_found < loclength)
 			    {
@@ -114,11 +94,32 @@ unsigned int findpattern(unsigned char *pattern,
 				locations->mode = mode;
 				locations++;
 			    }
-			    num_patterns_found ++;
+			    num_patterns_found++;
 			    num_char_match = 0;
 		        }
-		     }
-		  }
+		    }
+		    else    // pattern broke, but we need to check if
+			  // this is the start of a new pattern.
+		    {
+		        num_char_match = 0;
+		        if (*mem_ptr == pattern[num_char_match])
+		        {
+			    num_char_match++;
+
+		            if (num_char_match == patlength)
+		            {
+			        // add the pattern
+			        if (num_patterns_found < loclength)
+			        {
+				    locations->location = (unsigned int) (mem_ptr - patlength) + 1;
+				    locations->mode = mode;
+				    locations++;
+			        }
+			        num_patterns_found ++;
+			        num_char_match = 0;
+		            }
+		        }
+		    }
 		}
 
 	}
