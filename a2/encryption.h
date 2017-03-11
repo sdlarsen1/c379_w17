d6a365d07c4dcad0937dec0a5f9dcfb5392a28fa
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-11 14:49:26 -0700

    fixed linking

---
+++ a2/encryption.h
@@ -0,0 +1,4 @@
+char *base64encode (const void *b64_encode_this, int encode_this_many_bytes) ;
+char *base64decode (const void *b64_decode_this, int decode_this_many_bytes) ;
+const char * do_decrypt(char * in_msg) ;
+const char * do_crypt(char *in_msg) ;
