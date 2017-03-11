528b52e81d916a4d22857e487758bedb94a5d949
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-11 14:07:11 -0700

    created encryption.c + header

--- a2/test.c
+++ a2/test.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/evp.h>
-#include <openssl/pem.h>
 
 #define BUFFER_LEN 1024
 #define MESSAGE_LEN 512
@@ -45,35 +43,40 @@ char *base64decode (const void *b64_decode_this, int decode_this_many_bytes){
 
 const char * do_decrypt(char * in_msg) {
 
-    int bytes_to_decode = strlen(in_msg); //Number of bytes in string to base64 decode.
     int outlen, delen;
-    static unsigned char out_msg[BUFFER_LEN];
-    unsigned char *base64_decoded = base64decode(in_msg, bytes_to_decode);   //Base-64 decoding.
+    static unsigned char out_msg[MESSAGE_LEN], out_buff[MESSAGE_LEN];
     unsigned char key[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     unsigned char iv[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     EVP_CIPHER_CTX ctx;
 
+    int bytes_to_decode = strlen(in_msg);  // Number of bytes in string to base64 decode.
+    unsigned char *base64_decoded = base64decode(in_msg, bytes_to_decode);   // Base-64 decoding.
+
     outlen = strlen(base64_decoded);
-    free(in_msg);                //Frees up the memory holding our base64 encoded data.
 
     EVP_CIPHER_CTX_init(&ctx);
     EVP_DecryptInit_ex(&ctx, EVP_aes_256_cbc(), NULL, key, iv);
-    if(!EVP_DecryptUpdate(&ctx, out_msg, &delen, base64_decoded, outlen)) {
+    if(!EVP_DecryptUpdate(&ctx, out_buff, &delen, base64_decoded, outlen)) {
         /* Error */
+        printf("Error during decryption.\n");
         return 0;
     }
 
     int remainingBytes;
-    if(!EVP_DecryptFinal_ex(&ctx, out_msg + delen, &remainingBytes)) {
+    if(!EVP_DecryptFinal_ex(&ctx, out_buff + delen, &remainingBytes)) {
         /* Error */
+        printf("Error while decrypting the final block.\n");
         return 0;
     }
 
     delen += remainingBytes;
     EVP_CIPHER_CTX_cleanup(&ctx);
 
+    strncpy(out_msg, out_buff, delen);  // Remove extra padding
+
     return out_msg;
 }
+
 
 const char * do_crypt(char *in_msg) {
 
