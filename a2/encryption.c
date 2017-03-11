6f32d3e02210737c328020da167813980c9a537c
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-11 14:31:12 -0700

    playing with encrypt.h

--- a2/encryption.c
+++ a2/encryption.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
 #include "encryption.h"
 
 #define MESSAGE_LEN 512
--- a2/encryption.h
+++ a2/encryption.h
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <openssl/evp.h>
-#include <openssl/pem.h>
-
-// Function prototypes
-char *base64encode (const void *b64_encode_this, int encode_this_many_bytes) ;
-char *base64decode (const void *b64_decode_this, int decode_this_many_bytes) ;
-const char * do_decrypt(char * in_msg) ;
-const char * do_crypt(char *in_msg) ;
+char *base64encode (const void *b64_encode_this, int encode_this_many_bytes);
+char *base64decode (const void *b64_decode_this, int decode_this_many_bytes);
+const char * do_decrypt(char * in_msg);
+const char * do_crypt(char *in_msg);
