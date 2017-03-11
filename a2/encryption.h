d8705e5117e70dc33abff75a2c014d053698f44d
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-11 14:50:41 -0700

    update

--- a2/encryption.h
+++ a2/encryption.h
@@ -1,3 +1,7 @@
+/* Header for encryption.c */
+
+#define MESSAGE_LEN 512
+
 char *base64encode (const void *b64_encode_this, int encode_this_many_bytes) ;
 char *base64decode (const void *b64_decode_this, int decode_this_many_bytes) ;
 const char * do_decrypt(char * in_msg) ;
