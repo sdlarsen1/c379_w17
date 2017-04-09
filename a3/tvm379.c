0ac0c80855b7f814b46c55ce0bde590df79ec648
Stephen Larsen <sdlarsen@ualberta.ca>
2017-04-09 17:21:12 -0600

    added avs attribute to trace_file, created get_avg, update_avs

--- a3/tvm379.c
+++ a3/tvm379.c
@@ -5,15 +5,58 @@
 #include "page_table.h"
 #include "tlb.h"
 
+
 struct Trace_Files * create_trace_files(int num_tf) {
 	struct Trace_Files * trace_files;
-	trace_files = malloc(sizeof(struct Trace_Files));
-	trace_files->file_ptrs = malloc((sizeof(FILE *)) * (num_tf));
-	trace_files->tlbhits = malloc((sizeof(int)) * num_tf);
-	trace_files->pf = malloc((sizeof(int)) * num_tf);
-	trace_files->pageout = malloc((sizeof(int)) * num_tf);
+
+	trace_files = (struct Trace_Files *) malloc(sizeof(struct Trace_Files));
+	trace_files->file_ptrs = (FILE *) malloc((sizeof(FILE *)) * (num_tf));
+	trace_files->tlbhits = (int) malloc((sizeof(int)) * num_tf);
+	trace_files->pf = (int) malloc((sizeof(int)) * num_tf);
+	trace_files->pageout = (int) malloc((sizeof(int)) * num_tf);
+	trace_file->avs = (double *) malloc((sizeof(double *)) * num_tf);
+
+	for (int i = 0; i < num_tf; i++) {
+		trace_files->avs[i] = (double) malloc(sizeof(double));
+	}
 
 	return trace_files;
+}
+
+
+void destroy_trace_files(struct Trace_Files * trace_files, int num_tf) {
+	free(trace_files->file_ptrs);
+	free(trace_files->tlbhits);
+	free(trace_files->pf);
+	free(trace_files->pageout);
+
+	for (int i=0; i < num_tf, i++) {
+		free(trace_files->avs[i]);
+	}
+
+	free(trace_files->avs);
+	free(trace_files);
+}
+
+
+void update_avs(struct Trace_Files ** trace_files, int tf, double value) {  // pass in &trace_files
+	int avs_length = sizeof(trace_file->avs[tf]) / sizeof(double);
+
+	trace_files->avs[tf] = (double) realloc(trace_files->avs[tf], sizeof(double) * (avs_length+1));  // realloc memory
+
+	trace_files->avs[tf][avs_length+1] = value;
+}
+
+
+double get_avg(struct Trace_Files * trace_files, int tf) {
+	int num_values = sizeof(trace_files->avs[tf]) / sizeof(double);
+	int sum = 0;
+
+	for (int i = 0; i < num_values; i++) {
+		sum += trace_files->avs[tf][i];
+	}
+
+	return (sum / num_values);
 }
 
 
@@ -28,7 +71,6 @@ unsigned int get_value_from_tf(struct Trace_Files * trace_files, int tf, int off
 	} else {
 		return 0;
 	}
-
 }
 
 
@@ -91,11 +133,13 @@ int main(int argc, char *argv[]) {
 			final_entry += quantum;
 		}
 		// update avgs
+		update_avs(&trace_files, tf, value);
 	} while (!done);
 
 	// cleanup
 	destroy_page_table(page_table);
 	destroy_tlb(tlb);
+	destroy_trace_files(trace_files, num_tf);
 
 	// print+format values
 
