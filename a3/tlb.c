1abe404b0bf2ff007da699671e760961754b3a86
Hayden Bauder <bauder@cs.ualberta.ca>
2017-04-09 15:09:27 -0600

    update to tlb.h/c

--- a3/tlb.c
+++ a3/tlb.c
@@ -1,22 +1,82 @@
 #include <stdlib.h>
 #include "tlb.h"
 
+// update the LRU table
+void most_recently_used(struct TLB * tlb, int MRU)
+{
+	int MRU_rank = tlb->LRU_table[entry];
+	int entry;
+	for (entry = 0; entry < tlb->num_entries; entry++)
+	{
+		if (tlb->LRU_table[entry] <= MRU_rank)
+		{
+			tlb->LRU_table[entry]++;
+		}
+	}
+
+	tlb->LRU_table[MRU] = 0;	
+}
+
+// return index of entry that is LRU
+unsigned int get_LRU(struct TLB * tlb)
+{
+	int LRU, max = 0, entry;
+	for (entry = 0; entry < tlb->num_entries; entry++)
+	{
+		if (tlb->LRU_table[entry] > max)
+		{
+			LRU = entry;
+		}
+	}
+
+	return LRU;
+}
+
 struct TLB * create_tlb(int tlbentries, char mode)
 {
 	struct TLB * tlb;
-	tlb = malloc(sizeof struct TLB);
-	tlb->page_table = malloc((sizeof int) * tlbentries);
+	tlb = (struct TLB *) malloc(sizeof struct TLB);
+	tlb->page_table = (unsigned int *) malloc((sizeof int) * tlbentries);
+
 	if (mode == 'p')
 		tlb->ASID_table = NULL;
-	else
-		tlb->ASID_table = malloc((sizeof int) * tlbentries);
 
-	tlb->LRU_table = malloc((sizeof int) * tlbentries);
+	else
+		tlb->ASID_table = (unsigned int *) malloc((sizeof int) * tlbentries);
+
+	tlb->LRU_table = (unsigned int *) malloc((sizeof int) * tlbentries);
+	memset(tlb->LRU_table, 0, (sizeof int) * tlbentries);
 
 }
 
-int query_entry(struct TLB * tlb, unsigned int pagenum);
+int query_entry_tlb(struct TLB * tlb, unsigned int pagenum, int asid)
+{
+	int i;
+	for(i = 0; i < tlb->num_entries; i++)
+	{
+		if (tlb->page_table[i] == pagenum)
+		{
+			if (tlb->ASID_table == NULL)
+				return 1;	// found the entry
 
-void add_entry(struct TLB * tlb, int pagenum, int asid);
+			else
+			{
+				if (tlb->ASID_table[i] == asid)
+					return 1; // found the entry
+			}
+		}
+	}
 
-void destroy_tlb(struct TLB * tlb);
+	return 0;	// not in tlb
+}
+
+void add_entry_tlb(struct TLB * tlb, unsigned int pagenum, int asid);
+
+
+void destroy_tlb(struct TLB * tlb)
+{
+	free(tlb->page_table);
+	free(tlb->ASID_table);
+	free(tlb->LRU_table);
+	free(tlb);
+}
